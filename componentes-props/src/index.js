e13066275a54eecbe7f0ce2de72d6fba6b8dffd3
Izabelle Cesario <izabellecesario@hotmail.com>
2024-08-30 16:05:14 -0300

    feat(ap04-react): encerra apostila 04

--- componentes-props/src/index.js
+++ componentes-props/src/index.js
@@ -1,48 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Pedido from './Pedido';
-
+import React from 'react'
+import ReactDOM from  'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
 const App = () => {
-    return (
-        <div className='container border mt-2 py-5'>
-            <div className='row'>
-                <div className='col-12'>
-                    <h1 className='display-5 text-center'>Seus Pedidos</h1>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-sm-12 col-lg-6 col-xl-4 border py-5'>
-                    <Pedido 
-                        data='23/04/2024'
-                        icone='fas fa-hard-drive fa-3x'
-                        titulo='SSD'
-                        descricao='SSD Kington 240GB'
-                    />
-                </div>
-                <div className='col-sm-12 col-lg-6 col-xl-4 border py-5'>
-                    <Pedido 
-                        data='24/05/2024'
-                        icone='fa-solid fa-computer-mouse fa-3x'
-                        titulo='Mouse'
-                        descricao='Mouse Gamer Logitech G502'
-                    />
-                </div>
-                <div className='col-sm-12 col-lg-12 col-xl-4 border py-5'>
-                    <Pedido 
-                        data='25/06/2024'
-                        icone='fa-solid fa-memory fa-3x'
-                        titulo='Memória'
-                        descricao='Pente de memória DDR4 8GB'
-                    />
-                </div>
-            </div>
-            <div className='row'>
-                
-            </div>
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Ainda não chegou'
+  const funcaoOK = () => alert('Agradecemos a confirmação')
+  const funcaoNOK = () => alert('Verificaremos o ocorrido')
+  const componenteFeedback = 
+    <Feedback textoOK={textoOK} textoNOK={textoNOK} funcaoOK={funcaoOK} funcaoNOK={funcaoNOK}/>
+  return (
+    <div className='container border mt-2 py-5'>
+      <div className='row'>
+        <div className='col-12'>
+          <h1 className='display-5 text-center'>Seus pedidos</h1>
         </div>
-    )
+      </div>
+      <div className='row'>
+        <div className='col-sm-12 col-lg-6 col-xl-4 border py-5'>
+          <Cartao cabecalho="22/04/2024">
+            <Pedido 
+              icone="fa-solid fa-hard-drive fa-shake fa-3x"
+              titulo="SSD"
+              descricao="SSD Kingston"
+            />
+            {componenteFeedback}
+          </Cartao>
+        </div>
+        <div className='col-sm-12 col-lg-6 col-xl-4 border  py-5'>
+          <Cartao cabecalho="24/05/2024">
+            <Pedido 
+              icone="fa-solid fa-computer-mouse fa-beat fa-3x"
+              titulo="Mouse"
+              descricao="Microsoft Mouse 3500"
+            />
+            {componenteFeedback}
+          </Cartao>
+        </div>
+        <div className='col-sm-12 col-lg-12 col-xl-4 border  py-5'>
+          <Cartao cabecalho="25/05/2024">
+            <Pedido 
+              icone="fa-solid fa-memory fa-fade fa-3x"
+              titulo="Memória RAM"
+              descricao="Memoria RAM de 16Gb"
+            />
+            {componenteFeedback}
+          </Cartao>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
